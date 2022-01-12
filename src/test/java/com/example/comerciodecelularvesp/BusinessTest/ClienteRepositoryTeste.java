90c7676c0f73079082aface9084b045fb6726f30
Pedrinho-o-brabo <pedrinho22cm@gmail.com>
2022-01-12 14:50:45 -0400

    add pack de teste

--- src/test/java/com/example/comerciodecelularvesp/BusinessTest/ClienteRepositoryTeste.java
+++ src/test/java/com/example/comerciodecelularvesp/BusinessTest/ClienteRepositoryTeste.java
@@ -1,4 +1,23 @@
 package com.example.comerciodecelularvesp.BusinessTest;
 
+import com.example.comerciodecelularvesp.entities.Cliente;
+import com.example.comerciodecelularvesp.repositories.ClienteRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 public class ClienteRepositoryTeste {
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Test
+    public void findByAtivoTeste(){
+        List<Cliente> lista = clienteRepository.findByAtivo(true);
+        Boolean result = !lista.isEmpty();
+        assertThat(true).isEqualTo(result);
+    }
 }
