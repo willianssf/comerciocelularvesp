cac1bc8934396a491a93db7c8dca072e3eb91ca4
Pedrinho-o-brabo <pedrinho22cm@gmail.com>
2022-01-12 14:22:14 -0400

    ar

---
+++ src/main/java/com/example/comerciodecelularvesp/entities/Celular.java
@@ -0,0 +1,94 @@
+package com.example.comerciodecelularvesp.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ceular")
+public class Celular {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", length = 10, nullable = false)
+    private Integer id;
+
+    @Column(name="nome", nullable = false, length = 80)
+    private String nome;
+
+    @Column(name = "ativo", nullable = false)
+    private Boolean ativo;
+
+    @Column(name = "valor", length = 18, nullable = false)
+    private Double valor;
+
+    @Column(name = "id_cliente", length = 10, nullable = false)
+    private Integer idCliente;
+
+    @Column(name = "modelo", nullable = false, length = 50)
+    private String modelo;
+
+    @Column(name = "preco", length = 18, nullable = false)
+    private Double preco;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+
+
+
+
+
+
+}
