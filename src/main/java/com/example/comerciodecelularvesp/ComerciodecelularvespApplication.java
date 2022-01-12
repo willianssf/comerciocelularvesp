5294c1e4eefa55fd9b8b8c5846b2e40930ff5d08
willianssf <willian.silva552@gmail.com>
2022-01-12 14:01:45 -0400

    configuracao do projeto spring web pronto

---
+++ src/main/java/com/example/comerciodecelularvesp/ComerciodecelularvespApplication.java
@@ -0,0 +1,13 @@
+package com.example.comerciodecelularvesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComerciodecelularvespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComerciodecelularvespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/comerciodecelularvesp/ComerciodecelularvespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comerciodecelularvesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComerciodecelularvespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
