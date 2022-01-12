e175b1c9c126f80986019c7f3c2bae4ce3861a6f
narciza-fialho <narciza.fialho@hotmail.com>
2022-01-12 14:27:05 -0400

    Pedido controller

--- src/main/java/com/example/comerciodecelularvesp/controller/PedidoController.java
+++ src/main/java/com/example/comerciodecelularvesp/controller/PedidoController.java
@@ -3,10 +3,7 @@ package com.example.comerciodecelularvesp.controller;
 import com.example.comerciodecelularvesp.entities.Pedido;
 import com.example.comerciodecelularvesp.repositories.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,11 @@ public class PedidoController {
     public List<Pedido> listar() {
         List<Pedido> lista = pedidoRepository.findByAtvio(true);
         return lista;
+    }
+
+    @GetMapping("/{id}")
+    public Pedido buscar(@PathVariable int id) {
+        Pedido pedido = pedidoRepository.findById(id).get();
+        return pedido;
     }
 }
